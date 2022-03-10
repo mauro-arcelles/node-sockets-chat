832f09b970518e8fd9edc9517c2739b0c3899573
mauroar1001 <83997911+mauro-arcelles@users.noreply.github.com>
2022-03-10 18:24:35 -0500

    Chat finalizado con vista front

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -2,22 +2,26 @@ var socket = io();
 
 var params = new URLSearchParams(window.location.search);
 
-var usuario = {
-    nombre: params.get('nombre'),
-    sala: params.get('sala'),
-};
-
 if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
     throw new Error('El nombre y sala son necesarios');
 }
 
+var usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+};
+
+
+
 socket.on('connect', function () {
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function (resp) {
-        console.log('usuarios conectado', resp);
+        // console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
+
 });
 
 // escuchar
@@ -30,28 +34,28 @@ socket.on('disconnect', function () {
 
 // Enviar información
 // socket.emit('crearMensaje', {
-//     usuario: 'Fernando',
+//     nombre: 'Fernando',
 //     mensaje: 'Hola Mundo'
-// }, function (resp) {
+// }, function(resp) {
 //     console.log('respuesta server: ', resp);
 // });
 
 // Escuchar información
 socket.on('crearMensaje', function (mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    // console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
 // Escuchar cambios de usuarios
 // cuando un usuario entra o sale del chat
 socket.on('listaPersona', function (personas) {
-
-    console.log(personas);
-
+    renderizarUsuarios(personas);
 });
 
-// mensajes privados
+// Mensajes privados
 socket.on('mensajePrivado', function (mensaje) {
-    console.log('mensaje privado: ', mensaje);
+
+    console.log('Mensaje Privado:', mensaje);
+
 });
